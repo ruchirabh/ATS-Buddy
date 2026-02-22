9d3d84e66c1ec339861e60a93aa4fb5d70c80a67
B H Ruchira <bhruchira15@gmail.com>
2026-02-22 23:48:37 +0530

    UPDATED RESUME INIT

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 from app.routes.health_routes import health_bp
 from app.routes.auth_routes import auth_bp
+from app.routes.resume_routes import resume_bp
 
 # Register all blueprints here
 def register_blueprints(app):
     app.register_blueprint(health_bp)
     app.register_blueprint(auth_bp)
+    app.register_blueprint(resume_bp)
